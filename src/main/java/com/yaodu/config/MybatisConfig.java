c2fd4b9848d58ffa2f379c9e5db9479938330b04
gjf@0701 <jianfeng.gai@pharmacodia.com>
2019-09-17 11:54:14 +0800

    修改mybatis配置

--- src/main/java/com/yaodu/config/MybatisConfig.java
+++ src/main/java/com/yaodu/config/MybatisConfig.java
@@ -1,25 +1,28 @@
 package com.yaodu.config;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
+import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.util.StringUtils;
 
 @Configuration
-public class MybatisConfig implements ApplicationContextAware {
+@MapperScans(value = {@MapperScan("com.yaodu.mapper")})
+public class MybatisConfig {
 
-    @Value("${mybatis.base-package}")
-    private String basePackage;
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-//        MapperScannerConfigurer mapperScannerConfigurer = applicationContext.getBean(MapperScannerConfigurer.class);
-//        if(StringUtils.isEmpty(this.basePackage)){
-//            return;
-//        }
-//        mapperScannerConfigurer.setBasePackage(this.basePackage);
+    @Bean
+    @Primary
+    public MybatisProperties mybatisProperties(){
+        MybatisProperties properties = new MybatisProperties();
+        properties.setConfigLocation("classpath:mybatis/mybatis-config.xml");
+        properties.setTypeAliasesPackage("com.yaodu.entity");
+        return properties;
     }
 }
